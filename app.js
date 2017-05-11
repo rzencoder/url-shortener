df47f90a5bf8af6a815198f7c74f33039aee7292
adam <hamer88@hotmail.co.uk>
2017-05-11 11:27:45 +0100

    style changes

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 //Requirements
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-require('dotenv').config();
 const bodyParser = require('body-parser');
 const shortUrl = require('./models/shortUrl');
 const validUrl = require('valid-url');
 
 const app = express();
-var url = process.env.MONGOLAB_URI;
 
-mongoose.connect(url || 'mongodb://localhost/shortUrls');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/shortUrls');
 
-app.set('view engine', 'pug')
-app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'pug');
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index')
@@ -22,7 +21,6 @@ app.get('/', function (req, res) {
 //Add url to database and give a short randon number as new url
 app.get('/url/*', function (req, res) {
   let long = req.url.replace('/url/', '');
-
   if(validUrl.isUri(long)){
     let shortVal = Math.floor(Math.random()*10000).toString();
     let data = new shortUrl(
@@ -36,7 +34,7 @@ app.get('/url/*', function (req, res) {
         return res.send('Error adding to database')
       }
     })
-    return res.json({data});
+    return res.json(data);
   }
   else{
     return res.send('Invalid Url');
@@ -46,12 +44,11 @@ app.get('/url/*', function (req, res) {
 app.get('/favicon.ico', function(req, res) {
     res.sendStatus(204);
 });
+
 //Redirect to old url in database
 app.get('/:newUrl', function(req, res){
   var newUrl = req.params.newUrl;
-  console.log(newUrl)
   shortUrl.findOne({'smallUrl': newUrl}, function(error, data){
-    console.log(data)
     if(error){
       return res.send('Database Error');
     }
